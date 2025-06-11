3d534ffda5fab1a5588d133d75813426e8ab0eb6
fannyyrr29 <s160422005@student.ubaya.ac.id>
2025-06-11 10:47:44 +0700

    init

---
+++ app/src/main/java/com/ubaya/anmp_expensetracker/view/LoginFragment.kt
@@ -0,0 +1,27 @@
+package com.ubaya.anmp_expensetracker.view
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.ubaya.anmp_expensetracker.R
+
+
+class LoginFragment : Fragment() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+
+
+}
